aa9829317d0a627bc3e529b69279a7988b4628da
laralee3 <me@laralee.me>
2017-12-22 22:34:41 -0800

    Update deploy command to allow deployment to gh pages
    gulp deploy now builds and deploys contents of dist
    to the github pages branch

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ From project root:
 
 Outputs build files to dist folder and automatically serves. Will refresh browser with changes when src files are modified.
 
+## Deployment
+`gulp deploy`
+
+Builds to the dist folder and pushes contents of the build folder to the remote gh-pages branch, which is automatically build and served to http://ffxivarchon.org/.
+
+## Domain
+Added custom domain name to repo settings, added CNAME file in root, and updated DNS in google domain's Custom Resource settings.
--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,21 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	autoprefixer = require('gulp-autoprefixer'),
 	del = require('del'),
-	reload = browserSync.reload;
+	reload = browserSync.reload,
+	ghPages = require('gulp-gh-pages');
 
 gulp.task('assets', function() {
 	return gulp.src('./src/assets/**/*')
 		.pipe(gulp.dest('./dist/assets'))
-		.pipe(reload({stream:true}));
+		.pipe(reload({stream: true}));
 });
 
 gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: "./dist"
-        }
-    });
+	browserSync({
+		server: {
+			baseDir: "./dist"
+		}
+	});
 });
 
 gulp.task('clean', function(done) {
@@ -31,7 +32,7 @@ gulp.task('clean', function(done) {
 gulp.task('html', function() {
 	return gulp.src('./src/index.html')
 		.pipe(gulp.dest('./dist'))
-		.pipe(reload({stream:true}));
+		.pipe(reload({stream: true}));
 });
 
 gulp.task('sass', function() {
@@ -42,12 +43,20 @@ gulp.task('sass', function() {
 			cascade: false
 		}))
 		.pipe(gulp.dest('./dist'))
-		.pipe(reload({stream:true}));
+		.pipe(reload({stream: true}));
 });
 
-gulp.task('serve', ['browser-sync', 'sass', 'html', 'assets'], function() {
+gulp.task('build', ['sass', 'html', 'assets']);
+
+gulp.task('deploy', ['build'], function() {
+	return gulp.src('./dist/**/*')
+		.pipe(ghPages());
+});
+
+gulp.task('serve', ['browser-sync', 'build'], function() {
 	gulp.watch("src/assets/**/*", ['assets'], function() {reload();});
 	gulp.watch("src/index.html", ['html'], function() {reload();});
 	gulp.watch("src/main.scss", ['sass'], function() {reload();});
 });
+
 gulp.task('default', ['clean', 'serve']);
