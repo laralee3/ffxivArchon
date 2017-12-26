8db3219aa9a955182e43ccbef021d7396788939c
laralee3 <me@laralee.me>
2017-12-25 19:24:39 -0800

    Add main page gallery and rotating image
    Couldn't get scaling timing to sync through jquery,
    so added a slow enough keyframe animation that it
    should still look decent. Hopefully.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ $(function() {
 
     // Jquery object references
     var content = $('.content');
+    var gallery = $('.gallery');
     var intro = $('.intro');
     var menu = $('.menu');
     var navigation = $('.navigation');
@@ -18,15 +19,19 @@ $(function() {
     };
 
     var close = navigation.find('.close');
+    var galleryimage = gallery.find('.gallery-image');
     var hamburger = menu.find('.hamburger');
-
     var links = navigation.find('.link');
 
     // Jquery classnames, strings, etc.
     var visible = 'visible';
 
+    // state
+    var imageNumber = 1; // Crappy gallery, but a start
+    var totalImages = 10;
+
     ////////////////////////////////////////////////////
-    // Functions
+    // Functionality
     ////////////////////////////////////////////////////
 
     function toggleMenu() {
@@ -53,6 +58,19 @@ $(function() {
         toggleMenu();
     }
 
+    function galleryAnimation() {
+        galleryimage.css('background-image', 'url(\'assets/gallery/' + imageNumber + '.png\')');
+        imageNumber++;
+
+        if (imageNumber > totalImages) {
+            imageNumber = 1;
+        }
+
+        gallery.animate({opacity: 1}, 1000).delay(7000).animate({opacity: 0}, 2000, function() {
+            galleryAnimation();
+        });
+    }
+
     ////////////////////////////////////////////////////
     // Init
     ////////////////////////////////////////////////////
@@ -66,10 +84,10 @@ $(function() {
         setTimeout(function() {
             intro.hide();
             views.home.add(content).add(menu).addClass(visible);
+            galleryAnimation();
         }, 250);
     }, 1500); // Matches animation cycle for intro
 
-    // Functionality
     hamburger.add(close).on('click', function() {
         toggleMenu();
     });
